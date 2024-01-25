a6883be24013ec8267402dee133d1a8b26e3f20d
MrPuxe <anthondeleonsketch@gmail.com>
2024-01-25 11:34:40 -0500

    First commit with instructions

---
+++ Adding_And_Removing_Dots.py
@@ -0,0 +1,8 @@
+#Function 'add_dots' takes a string and adds "." in between each letter.
+    #E.g, calling add_dots("test") should return the string "t.e.s.t".
+
+#Function 'remove_dots' removes all dots from a string. 
+    #E.g, calling remove_dots("t.e.s.t") should return "test".
+
+#By Calling both Functions remove_dots(add_dots(string))
+#should return back te original string for any string.
